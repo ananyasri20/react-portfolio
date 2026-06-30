bd3b4917028315fb4b6b1ffec3e7115432ef07a7
Ananya Srivastava <your-email@example.com>
2026-07-01 00:27:21 +0530

    Fix build issues

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
   return (
     <section
     id="about"
-     className="c-space section-spacing" id="about">
+     className="c-space section-spacing" >
       <h2 className="text-heading">About Me</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
         {/* Grid 1 */}
